45510c5d623fbe655570e2d0b9e9ce331618f418
Claud-Yx <80788446+Claud-Yx@users.noreply.github.com>
2021-09-09 19:48:05 +0900

    DRILL 03 완성
    2019180048 윤여건

---
+++ name.py
@@ -0,0 +1,85 @@
+# [W02] DRILL#3 'name.py' - 2019180048 윤여건
+import turtle
+
+turtle.speed(15)
+
+#ㅇ
+turtle.circle(30)
+turtle.penup()
+
+#ㅠ
+turtle.goto(-60,-25)
+turtle.pendown()
+turtle.forward(120)
+turtle.penup()
+turtle.goto(-20, -25)
+turtle.pendown()
+turtle.right(90)
+turtle.forward(30)
+turtle.penup()
+turtle.goto(20, -25)
+turtle.pendown()
+turtle.forward(30)
+turtle.penup()
+
+#ㄴ
+turtle.goto(-40, -40)
+turtle.pendown()
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(90)
+turtle.penup()
+
+#ㅇ
+turtle.goto(90,0)
+turtle.right(90)
+turtle.pendown()
+turtle.circle(30)
+turtle.penup()
+
+#ㅕ
+turtle.goto(160, 15)
+turtle.left(90)
+turtle.pendown()
+turtle.forward(30)
+turtle.penup()
+turtle.goto(160, -15)
+turtle.pendown()
+turtle.forward(30)
+turtle.penup()
+turtle.goto(190, 60)
+turtle.right(90)
+turtle.pendown()
+turtle.forward(120)
+turtle.penup()
+
+#ㄱ
+turtle.goto(220, 30)
+turtle.left(90)
+turtle.pendown()
+turtle.forward(60)
+turtle.right(120)
+turtle.forward(70)
+turtle.penup()
+
+#ㅓ
+turtle.goto(280, 0)
+turtle.left(120)
+turtle.pendown()
+turtle.forward(30)
+turtle.penup()
+turtle.goto(310, 60)
+turtle.right(90)
+turtle.pendown()
+turtle.forward(120)
+turtle.penup()
+
+#ㄴ
+turtle.goto(250, -40)
+turtle.pendown()
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(90)
+turtle.penup()
+
+turtle.exitonclick()
